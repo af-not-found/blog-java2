e5163d25a53abe3541706e78e880ee7f1230e83d
afnf <af.not.found@gmail.com>
2015-05-17 23:52:03 +0900

    fix error page/json

--- src/main/java/net/afnf/blog/web/GlobalControllerExceptionHandler.java
+++ src/main/java/net/afnf/blog/web/GlobalControllerExceptionHandler.java
@@ -1,38 +1,60 @@
 package net.afnf.blog.web;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import net.afnf.blog.common.JsonResponseDemoSiteErrorException;
 import net.afnf.blog.common.JsonResponseException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 class GlobalDefaultExceptionHandler {
 
     private static Logger logger = LoggerFactory.getLogger(GlobalDefaultExceptionHandler.class);
 
-    @ExceptionHandler(value = Exception.class)
-    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+    @Value("${server.context-path}")
+    private String contextPath;
 
-        String viewName = "redirect:/";
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public String defaultErrorHandler(HttpServletRequest req, HttpServletResponse res, Exception e) throws Exception {
+
+        String url = req != null ? req.getRequestURI() : "";
+        String estr = "url=" + url + ", e=" + e.getClass().getName();
+        boolean jsondemo = false;
+        boolean jsonreq = (req != null && req.getHeader("X-Requested-With") != null) ? true : false;
+
         if (e instanceof JsonResponseDemoSiteErrorException) {
-            viewName = "error_demosite_json";
-            logger.warn(e.getClass().getName());
+            jsondemo = true;
+            logger.debug(estr);
         }
         else if (e instanceof JsonResponseException) {
-            logger.warn(e.getClass().getName());
+            logger.warn(estr);
+        }
+        else if (e instanceof TypeMismatchException || e instanceof BindException) {
+            logger.info(estr);
         }
         else {
-            logger.warn("", e);
+            logger.warn("url=" + url, e);
         }
 
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName(viewName);
-        return mav;
+        if (jsondemo || jsonreq) {
+            res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            res.setContentType("application/json; charset=UTF-8");
+            return jsondemo ? "{\"demoSite\":true}" : "{\"error\":true}";
+        }
+        else {
+            res.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+            res.setHeader("Location", contextPath);
+            return "";
+        }
     }
 }
