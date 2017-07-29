8bb22870367de39c4f660891f9ed9d64e765508d
afnf <af.not.found@gmail.com>
2017-07-29 15:29:45 +0900

    update spring-boot to 1.5.4.RELEASE

--- src/main/java/net/afnf/blog/common/CachingResourceUrlEncodingFilter.java
+++ src/main/java/net/afnf/blog/common/CachingResourceUrlEncodingFilter.java
@@ -6,6 +6,8 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
@@ -15,6 +17,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
 import org.springframework.web.servlet.resource.ResourceUrlProvider;
 import org.springframework.web.servlet.resource.ResourceUrlProviderExposingInterceptor;
+import org.springframework.web.util.UrlPathHelper;
 
 public class CachingResourceUrlEncodingFilter extends ResourceUrlEncodingFilter {
 
@@ -30,9 +33,14 @@ public class CachingResourceUrlEncodingFilter extends ResourceUrlEncodingFilter 
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        filterChain.doFilter(request, new CachingResourceUrlEncodingResponseWrapper(request, response, encTargetPrefix));
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        if (!(request instanceof HttpServletRequest) || !(response instanceof HttpServletResponse)) {
+            throw new ServletException("ResourceUrlEncodingFilter just supports HTTP requests");
+        }
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        filterChain.doFilter(request, new CachingResourceUrlEncodingResponseWrapper(httpRequest, httpResponse, encTargetPrefix));
     }
 
     private static class CachingResourceUrlEncodingResponseWrapper extends HttpServletResponseWrapper {
@@ -99,13 +107,14 @@ public class CachingResourceUrlEncodingFilter extends ResourceUrlEncodingFilter 
 
         private void initLookupPath(ResourceUrlProvider urlProvider) {
             if (this.indexLookupPath == null) {
-                String requestUri = urlProvider.getPathHelper().getRequestUri(this.request);
-                String lookupPath = urlProvider.getPathHelper().getLookupPathForRequest(this.request);
+				UrlPathHelper pathHelper = urlProvider.getUrlPathHelper();
+				String requestUri = pathHelper.getRequestUri(this.request);
+				String lookupPath = pathHelper.getLookupPathForRequest(this.request);
                 this.indexLookupPath = requestUri.lastIndexOf(lookupPath);
                 this.prefixLookupPath = requestUri.substring(0, this.indexLookupPath);
 
                 if ("/".equals(lookupPath) && !"/".equals(requestUri)) {
-                    String contextPath = urlProvider.getPathHelper().getContextPath(this.request);
+					String contextPath = pathHelper.getContextPath(this.request);
                     if (requestUri.equals(contextPath)) {
                         this.indexLookupPath = requestUri.length();
                         this.prefixLookupPath = requestUri;
