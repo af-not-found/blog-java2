c4e357f5a3f7fc82192d7608953c176f7f4529c3
afnf <af.not.found@gmail.com>
2017-10-09 18:59:30 +0900

    migrate metrics architecture

--- src/main/java/net/afnf/blog/service/MetricsService.java
+++ src/main/java/net/afnf/blog/service/MetricsService.java
@@ -2,35 +2,118 @@ package net.afnf.blog.service;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.MutablePair;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.system.DiskSpaceHealthIndicator;
 import org.springframework.stereotype.Service;
-import org.springframework.boot.actuate.endpoint.*;
+
+import io.micrometer.core.instrument.Measurement;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.Meter.Id;
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+import io.micrometer.core.instrument.internal.MeterId;
 
 @Service
 public class MetricsService {
 
+    private static final Log logger = LogFactory.getLog(MetricsService.class);
+
     @Autowired
-    private Collection<PublicMetrics> publicMetrics;
+    private CompositeMeterRegistry compositeMeterRegistry;
+
+    @Autowired
+    private DiskSpaceHealthIndicator diskSpaceHealthIndicator;
 
     public List<Pair<String, String>> getMetricsList() {
 
         List<Pair<String, String>> list = new ArrayList<Pair<String, String>>();
 
-        Iterator<PublicMetrics> ite1 = publicMetrics.iterator();
-        while (ite1.hasNext()) {
-            PublicMetrics publicMetrics = (PublicMetrics) ite1.next();
-            Iterator<Metric<?>> ite2 = publicMetrics.metrics().iterator();
+        addHealthInfo("DiskSpace", list, diskSpaceHealthIndicator.health());
+
+        addMeterInfo(list, compositeMeterRegistry.getMeters());
+
+        Collections.sort(list, new Comparator<Pair<String, String>>() {
+            @Override
+            public int compare(Pair<String, String> obj1, Pair<String, String> obj2) {
+                return obj1.getKey().compareTo(obj2.getKey());
+            }
+        });
+
+        return list;
+    }
+
+    protected void addMeterInfo(List<Pair<String, String>> list, Collection<Meter> meteres) {
+        Iterator<Meter> iterator = meteres.iterator();
+        while (iterator.hasNext()) {
+            Meter meter = (Meter) iterator.next();
+            Id meterId = meter.getId();
+            if (meterId instanceof MeterId == false) {
+                logger.warn("non MeterId type : " + meterId);
+                continue;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            Iterator<Measurement> ite2 = meter.measure().iterator();
             while (ite2.hasNext()) {
-                Metric<?> metric = (Metric<?>) ite2.next();
-                list.add(new MutablePair<String, String>(metric.getName(), metric.getValue().toString()));
+                Measurement measurement = (Measurement) ite2.next();
+                sb.append(measurement.getValue());
+                if (ite2.hasNext()) {
+                    sb.append(",");
+                }
+            }
+            list.add(new MutablePair<String, String>(meterId2String((MeterId) meterId), sb.toString()));
+        }
+    }
+
+    protected String meterId2String(MeterId meterId) {
+        StringBuilder sb = new StringBuilder();
+        if (meterId.getTags() != null) {
+            Iterator<Tag> iterator = meterId.getTags().iterator();
+            while (iterator.hasNext()) {
+                Tag tag = iterator.next();
+                String tagkey = tag.getKey();
+                if (StringUtils.equals(tagkey, "id")) {
+                    sb.insert(0, ",");
+                    sb.insert(0, tag.getValue());
+                }
+                else if (StringUtils.equals(tagkey, "area") == false) {
+                    sb.append(tag.getKey()).append("=").append(tag.getValue()).append(",");
+                }
             }
         }
 
-        return list;
+        if (sb.length() != 0) {
+            sb.delete(sb.length() - 1, sb.length()); // remove last comma
+            sb.insert(0, "[");
+            sb.insert(0, meterId.getName());
+            sb.append("]");
+        }
+        else {
+            sb.append(meterId.getName());
+        }
+
+        return sb.toString();
+    }
+
+    protected void addHealthInfo(String prefix, List<Pair<String, String>> list, Health health) {
+        Map<String, Object> map = health.getDetails();
+        Iterator<String> ite1 = map.keySet().iterator();
+        while (ite1.hasNext()) {
+            String key = ite1.next();
+            Object object = map.get(key);
+            list.add(new MutablePair<String, String>(prefix + "." + key, object.toString()));
+        }
     }
 }
