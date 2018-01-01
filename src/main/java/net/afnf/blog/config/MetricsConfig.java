b28719d62b1db1dce4444118ebab0c25cdb7340c
afnf <af.not.found@gmail.com>
2018-01-02 03:21:57 +0900

    follow micrometer.io change

--- src/main/java/net/afnf/blog/config/MetricsConfig.java
+++ src/main/java/net/afnf/blog/config/MetricsConfig.java
@@ -1,43 +1,33 @@
 package net.afnf.blog.config;
 
-import java.util.Collection;
-
 import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.metrics.jdbc.DataSourcePoolMetrics;
-import org.springframework.boot.jdbc.metadata.DataSourcePoolMetadataProvider;
 import org.springframework.context.annotation.Configuration;
 
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.binder.ClassLoaderMetrics;
-import io.micrometer.core.instrument.binder.JvmGcMetrics;
-import io.micrometer.core.instrument.binder.JvmMemoryMetrics;
-import io.micrometer.core.instrument.binder.JvmThreadMetrics;
-import io.micrometer.core.instrument.binder.LogbackMetrics;
-import io.micrometer.core.instrument.binder.UptimeMetrics;
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.system.FileDescriptorMetrics;
 
 @Configuration
 class MetricsConfig {
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Autowired
-    private Collection<DataSourcePoolMetadataProvider> metadataProviders;
 
     @Autowired
     private MeterRegistry registry;
 
     @PostConstruct
     private void instrumentDataSource() {
-        new DataSourcePoolMetrics(dataSource, metadataProviders, "DataSource", null).bindTo(registry);
         new JvmThreadMetrics().bindTo(registry);
-        new JvmMemoryMetrics().bindTo(registry);
         new JvmGcMetrics().bindTo(registry);
-        new LogbackMetrics().bindTo(registry);
-        new UptimeMetrics().bindTo(registry);
         new ClassLoaderMetrics().bindTo(registry);
+        new FileDescriptorMetrics().bindTo(registry);
+        
+        // 以下のMetricsは自動追加
+        // DataSourcePoolMetrics
+        // JvmMemoryMetrics
+        // LogbackMetrics
+        // UptimeMetrics
     }
 }
