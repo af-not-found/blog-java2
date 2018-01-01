0d2e8ae59437a9f4bb215c81b9f8c683046efda2
afnf <af.not.found@gmail.com>
2018-01-02 03:24:07 +0900

    follow AbstractStandardExpressionAttributeTagProcessor change

--- src/main/java/net/afnf/blog/thymeleaf/processor/ProcessorUtil.java
+++ src/main/java/net/afnf/blog/thymeleaf/processor/ProcessorUtil.java
@@ -1,12 +1,10 @@
 package net.afnf.blog.thymeleaf.processor;
 
-import org.apache.commons.text.StringEscapeUtils;
 import org.thymeleaf.IEngineConfiguration;
 import org.thymeleaf.context.ITemplateContext;
 import org.thymeleaf.standard.expression.FragmentExpression;
 import org.thymeleaf.standard.expression.IStandardExpression;
 import org.thymeleaf.standard.expression.IStandardExpressionParser;
-import org.thymeleaf.standard.expression.NoOpToken;
 import org.thymeleaf.standard.expression.StandardExpressionExecutionContext;
 import org.thymeleaf.standard.expression.StandardExpressions;
 
@@ -23,27 +21,31 @@ public class ProcessorUtil {
 
         IStandardExpression expression = parser.parseExpression(context, attributeValue);
 
-        Object expressionResult = null;
+        final Object expressionResult;
         if (attributeValue != null) {
 
             if (expression != null && expression instanceof FragmentExpression) {
+                // This is merely a FragmentExpression (not complex, not combined with anything), so we can apply a shortcut
+                // so that we don't require a "null" result for this expression if the template does not exist. That will
+                // save a call to resource.exists() which might be costly.
 
                 final FragmentExpression.ExecutedFragmentExpression executedFragmentExpression = FragmentExpression
-                        .createExecutedFragmentExpression(context, (FragmentExpression) expression,
-                                StandardExpressionExecutionContext.NORMAL);
-
+                        .createExecutedFragmentExpression(context, (FragmentExpression) expression);
                 expressionResult = FragmentExpression.resolveExecutedFragmentExpression(context, executedFragmentExpression,
                         true);
             }
             else {
-                expressionResult = expression.execute(context);
+
+                /*
+                 * Some attributes will require the execution of the expressions contained in them in RESTRICTED
+                 * mode, so that e.g. access to request parameters is forbidden.
+                 */
+                final StandardExpressionExecutionContext expCtx = StandardExpressionExecutionContext.RESTRICTED;
+                expressionResult = expression.execute(context, expCtx);
             }
         }
-
-        if (expressionResult != null && expressionResult != NoOpToken.VALUE) {
-            if (escape && expressionResult instanceof String) {
-                expressionResult = StringEscapeUtils.escapeXml11((String) expressionResult);
-            }
+        else {
+            expressionResult = null;
         }
 
         return expressionResult;
